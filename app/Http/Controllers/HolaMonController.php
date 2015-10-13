9f555277c56c69bf9adc0cb14f7d212d8ee32938
adam Alvarado <adamalvarado@iesebre.com>
2015-10-13 21:57:52 +0200

    Change name controller

--- app/Http/Controllers/HolaMonController.php
+++ app/Http/Controllers/HolaMonController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
-class HolaMon extends Controller
+class HolaMonController extends Controller
 {
     public function hello()
     {

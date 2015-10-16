025a93360f6c2b567ad1d1167570963dc37071cf
vagrant <vagrant@homestead>
2015-10-16 15:28:43 +0000

    Added resource controller

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Hola Món!</div>
+                <div class="title">Hola Món! El meu nom és Adam</div>
             </div>
         </div>
     </body>
